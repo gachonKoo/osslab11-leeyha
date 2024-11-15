365578db1d31615878868a9cc2124f70dc95f962
leeyongha <youngiha0319@naver.com>
2024-11-16 02:13:03 +0900

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,10 @@
+import geo.utils as utils
+def pythagoras(a, b):
+    c = math.sqrt(a**2 + b**2)
+    return c
+    print('c=',c)
+
+def circle(r):
+    area = math.pi * r**2
+    return area
+    print('area=',area)
